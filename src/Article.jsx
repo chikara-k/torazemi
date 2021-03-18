adf0797ff5fa6ce66406983c953755c68bfc72f5
chikara-k <wabisabitea.c@gmail.com>
2021-03-19 08:44:15 +0900

    入門終了

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import LikeButton from "./LikeButton";
 
 // Function Componentの書き方
 // stateの取得　子コンポーネントで参照したい場合はpropsとして渡す
+// React HooksでFunctional component内でもstateを使えるようになった
 
 const Article = (props) => {
+  const[isPublished, togglePublished] = useState(false)
+
   return (
     <div>
       <h2>{props.title}</h2>
       <label htmlfor="check">公開状態:</label>
-      <input type="checkbox" checked={props.isPublished} id="check" onClick={() => props.toggle()}/>
-      <LikeButton count={props.count}/>
+      <input type="checkbox" checked={isPublished} id="check" onClick={() => togglePublished(!isPublished)}/>
+      <LikeButton />
     </div>
   )
 };
--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,59 +1,63 @@
 import React from 'react';
-import Article from "./Article"
+import Article from "./Article";
+import * as FooBar from "./components/FooBar";
+import Hoge from './components/Hoge';
 
 // Class Componentの書き方
 // stateの設定方法　constructor()内で宣言する　オブジェクト型で記述
+// class Blog extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isPublished: false,
+//       count: 0
+//     }
+//   }
+const Blog = () => {
 
-class Blog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isPublished: false,
-      count: 0
-    }
-  }
+  // componentDidMount() {
+  //   // ボタンがクリックされたらいいねをカウントアップする
+  //   document.getElementById('counter').addEventListener('click', this.countUp)
+  // }
 
-  componentDidMount() {
-    // ボタンがクリックされたらいいねをカウントアップする
-    document.getElementById('counter').addEventListener('click', this.countUp)
-  }
+  // componentDidUpdate(){
+  //   if (this.state.count >= 10) {
+  //       this.setState({count: 0})
+  //   }
+  // }
 
-  componentDidUpdate(){
-    if (this.state.count >= 10) {
-        this.setState({count: 0})
-    }
-  }
-
-  componentWillUnmount() {
-    document.getElementById('counter').removeEventListener('click', this.countUp)
-  }
+  // componentWillUnmount() {
+  //   document.getElementById('counter').removeEventListener('click', this.countUp)
+  // }
 
   // 公開状態を反転させる関数　　setState()で値を変更する 関数にラップするのが一般的
   // setState()内に記述されたstateのみを変更
-  togglePublished = () => {
-    this.setState({
-      isPublished: !this.state.isPublished
-    })
-  };
+  // togglePublished = () => {
+  //   this.setState({
+  //     isPublished: !this.state.isPublished
+  //   })
+  // };
 
-  countUp = () => {
-    this.setState({ count: this.state.count + 1 })
-  };
+  // countUp = () => {
+  //   this.setState({ count: this.state.count + 1 })
+  // };
 
   // renderの中で値の変更してはいけない
 　// stateの取得　同コンポーネント内ならthis.state.key名で取得　　子コンポーネントで参照したい場合はpropsとして渡す
-  render() {
+  // render() {
     return (
       <>
         <Article 
             title="Reactの使い方" 
-            isPublished={this.state.isPublished} 
-            toggle={() => this.togglePublished()}
-            count={this.state.count}
+            // isPublished={this.state.isPublished} 
+            // toggle={() => this.togglePublished()}
+            // count={this.state.count}
         />
+        <FooBar.Foo />
+        <FooBar.Bar />
+        <Hoge />
       </>
     )
-  }
 }
 
 export default Blog
